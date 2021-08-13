8d2c9e5083d18c3441dc00b5d33abb540fece2e0
Romulo <rjaalbuquerque@gmail.com>
2021-08-13 16:22:14 +0100

    Moved from env.py to .env

---
+++ envereras.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ['IP'] = '0.0.0.0'
+os.environ['PORT'] = '5000'
+os.environ['MONGODB_NAME'] = 'pawer'
+os.environ['MONGO_URI'] = 'mongodb+srv://root:R0mul0ng0u5@pawercluster-ykiju.mongodb.net/pawer?retryWrites=true&w=majority'
+os.environ['SECRET_KEY'] ="randomstring123"
+os.environ['SESSION_KEY'] = 'randomstring123'
