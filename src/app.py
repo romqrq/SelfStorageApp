3ea810b8a03fbe26a9f5fd895521927c3753e09f
Romulo <rjaalbuquerque@gmail.com>
2021-08-13 16:46:37 +0100

    Move static content to src

--- src/app.py
+++ src/app.py
@@ -86,4 +86,4 @@ def add_booking():
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"), port=os.environ.get("PORT"), debug=False)
+    app.run(host=os.environ.get("IP"), port=os.environ.get("PORT"), debug=True)
