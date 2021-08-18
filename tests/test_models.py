4f906059cb502914504fd4b9aba5200c7656c35d
Romulo <rjaalbuquerque@gmail.com>
2021-08-18 15:37:45 +0100

    Update formatting on test files

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,4 @@
-
-from src.models import Unit, Booking
+from src.models import Booking, Unit
 
 
 def test_new_unit():
@@ -8,9 +7,9 @@ def test_new_unit():
     WHEN creating a new unit
     THEN check key/values are defined correctly
     """
-    unit = Unit(unit_name='test unit', unit_size='20', unit_price=20.20)
-    assert unit.unit_name == 'test unit'
-    assert unit.unit_size == '20'
+    unit = Unit(unit_name="test unit", unit_size="20", unit_price=20.20)
+    assert unit.unit_name == "test unit"
+    assert unit.unit_size == "20"
     assert unit.unit_price == 20.20
 
 
@@ -20,12 +19,17 @@ def test_new_booking():
     WHEN creating a new booking
     THEN check key/values are defined correctly
     """
-    booking = Booking(user_name='user name', address='user address', email='user email', move_in_date='move in date', 
-                      unit=Unit(unit_name='test unit', unit_size='20', unit_price=20.20) )
-    assert booking.user_name == 'user name'
-    assert booking.address == 'user address'
-    assert booking.email == 'user email'
-    assert booking.move_in_date == 'move in date'
-    assert booking.unit.unit_name == 'test unit'
-    assert booking.unit.unit_size == '20'
+    booking = Booking(
+        user_name="user name",
+        address="user address",
+        email="user email",
+        move_in_date="move in date",
+        unit=Unit(unit_name="test unit", unit_size="20", unit_price=20.20),
+    )
+    assert booking.user_name == "user name"
+    assert booking.address == "user address"
+    assert booking.email == "user email"
+    assert booking.move_in_date == "move in date"
+    assert booking.unit.unit_name == "test unit"
+    assert booking.unit.unit_size == "20"
     assert booking.unit.unit_price == 20.20
